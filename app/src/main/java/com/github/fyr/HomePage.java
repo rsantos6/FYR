a7851dbbddd363180d0768c8f0c8d7cbb3c3abd9
Alec Rodgers <alecr1997@gmail.com>
2016-11-06 17:25:29 -0500

    Added profile page with edit biography / profile picture functionality

--- app/src/main/java/com/github/fyr/HomePage.java
+++ app/src/main/java/com/github/fyr/HomePage.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class HomePage extends AppCompatActivity {
@@ -31,5 +32,12 @@ public class HomePage extends AppCompatActivity {
         Intent intent = new Intent(this, WhatPace.class);
         startActivity(intent);
     }
+
+    public void toProfilePage(View view) {
+        Intent i = new Intent(this, ProfilePage.class);
+        startActivity(i);
+    }
+
+
 
 }
---
+++ app/src/main/java/com/github/fyr/ProfilePage.java
@@ -0,0 +1,82 @@
+package com.github.fyr;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+public class ProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+    }
+
+    public void editBio(View view) {
+        AlertDialog.Builder editBioAlert = new AlertDialog.Builder(this);
+
+        editBioAlert.setTitle("Edit Biography");
+        editBioAlert.setMessage("250 character limit");
+
+        final EditText input = new EditText(this);
+        editBioAlert.setView(input);
+
+        editBioAlert.setPositiveButton("Submit", new Dialog.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String value = input.getText().toString();
+                TextView bioText = (TextView) findViewById(R.id.bioText);
+                bioText.setText(value);
+            }
+        });
+
+        editBioAlert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+
+        editBioAlert.show();
+    }
+
+    public void editProfilePicture(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        int pickImageRequest = 1;
+
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), pickImageRequest);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                ImageView imageView = (ImageView) findViewById(R.id.profilePicture);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
