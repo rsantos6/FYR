4d3a5f1d2def20785590a947c6546c06d73c847c
Russell Santos <russellandshira@msn.com>
2016-11-05 21:51:12 -0400

    Made basic flow from first page to a home page to selecting run preferences for that day. Have no implemented login or authentication.

--- app/src/main/java/com/github/fyr/HomePage.java
+++ app/src/main/java/com/github/fyr/HomePage.java
@@ -1,6 +1,5 @@
 package com.github.fyr;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,19 +7,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.Window;
 
-public class MainActivity extends Activity {
+public class HomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);//gets rid of title bar
-        setContentView(R.layout.activity_main);
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_home_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,8 +27,8 @@ public class MainActivity extends Activity {
         });
     }
 
-    public void toHome(View view){
-        Intent intent = new Intent(this, HomePage.class);
+    public void toPace(View view){
+        Intent intent = new Intent(this, WhatPace.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/github/fyr/WhatPace.java
+++ app/src/main/java/com/github/fyr/WhatPace.java
@@ -1,26 +1,20 @@
 package com.github.fyr;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.Window;
 
-public class MainActivity extends Activity {
+public class WhatPace extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);//gets rid of title bar
-        setContentView(R.layout.activity_main);
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_what_pace);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -30,11 +24,6 @@ public class MainActivity extends Activity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    public void toHome(View view){
-        Intent intent = new Intent(this, HomePage.class);
-        startActivity(intent);
     }
 
 }
